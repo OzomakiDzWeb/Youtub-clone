c393037de85b5f1cfd629934b02f021ba82d2e59
OzomakiDzWeb <ayoubbouslama1994@gmail.com>
2023-08-21 05:24:10 +0100

    creat card Video

--- src/Component/VideoDetails.js
+++ src/Component/VideoDetails.js
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { context } from '../Context/contextApi'
+import { useParams } from 'react-router'
+import {fetchDataApi} from '../Utiltis/Api'
 
 const VideoDetails = () => {
+  const [video,setvideo]=useState()
+  const [relatedVideo,setRelatedVideo]=useState()
+  const {id}=useParams()
+  const {setLoding}=useContext(context)
+  useEffect(()=>{
+fetchVideoDetails()
+  },[id])
+  const fetchVideoDetails=()=>{
+    setLoding(true)
+    fetchDataApi(`video/details/?id=${id}`).then(res=>{
+      console.log(res)
+      setvideo(res)
+      setLoding(false)
+    })
+  }
+  
   return (
     <div>VideoDetails</div>
   )
--- src/Utiltis/Api.js
+++ src/Utiltis/Api.js
@@ -7,7 +7,7 @@ const options = {
     gl: 'US'
   },
   headers: {
-    'X-RapidAPI-Key':process.env.REACT_APP_YOUTYB_API_KEY,
+    'X-RapidAPI-Key':'b689e6629dmsh3f37452b2e575f5p16ddc9jsn1b2eacbcb03e',
     'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
   }
 };
